3b34265eb5034a5191b7137eb17e548728994f53
Dina Orlova <dina.turilova@gmail.com>
2021-01-25 12:40:12 +0300

    Tests changed

--- src/test/java/ru/netology/web/CardOrderInvalidTest.java
+++ src/test/java/ru/netology/web/CardOrderInvalidTest.java
@@ -1,6 +1,7 @@
 package ru.netology.web;
 
 import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.exactText;
@@ -10,82 +11,96 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class CardOrderInvalidTest {
 
-    @Test
-    void shouldTestFormIsEmpty() {
+    @BeforeEach
+    void setup() {
         open("http://localhost:9999/");
-        SelenideElement form = $(".form.form.form_size_m.form_theme_alfa-on-white");
-        form.$("button").click();
-        $(".input_invalid").$(".input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
     }
 
     @Test
-    void shouldTestNameInLatin() {
-        open("http://localhost:9999/");
-        SelenideElement form = $(".form.form.form_size_m.form_theme_alfa-on-white");
+    void shouldGetErrorWhenNameIsEmpty() {
+        SelenideElement form = $(".form");
+        form.$("[data-test-id=phone] input").setValue("+79878888888");
+        form.$("[data-test-id=agreement]").click();
+        form.$("button").click();
+        $("[data-test-id=name].input_invalid .input__sub")
+                .shouldHave(exactText("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    void shouldGetErrorIfNameInLatin() {
+        SelenideElement form = $(".form");
         form.$("[data-test-id=name] input").setValue("Petrov Dima");
+        form.$("[data-test-id=phone] input").setValue("+79878888888");
+        form.$("[data-test-id=agreement]").click();
         form.$("button").click();
-        $(".input_invalid").$(".input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+        $("[data-test-id=name].input_invalid .input__sub")
+                .shouldHave(exactText("Имя и Фамилия указаные неверно." +
+                        " Допустимы только русские буквы, пробелы и дефисы."));
     }
 
     @Test
-    void shouldTestNameInNumerals() {
-        open("http://localhost:9999/");
-        SelenideElement form = $(".form.form.form_size_m.form_theme_alfa-on-white");
+    void shouldGetErrorIfNameInNumerals() {
+        SelenideElement form = $(".form");
         form.$("[data-test-id=name] input").setValue("535739792");
+        form.$("[data-test-id=phone] input").setValue("+79878888888");
+        form.$("[data-test-id=agreement]").click();
         form.$("button").click();
-        $(".input_invalid").$(".input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+        $("[data-test-id=name].input_invalid .input__sub")
+                .shouldHave(exactText("Имя и Фамилия указаные неверно." +
+                        " Допустимы только русские буквы, пробелы и дефисы."));
     }
 
     @Test
-    void shouldTestWithoutPhone() {
-        open("http://localhost:9999/");
-        SelenideElement form = $(".form.form.form_size_m.form_theme_alfa-on-white");
+    void shouldGetErrorWhenPhoneIsEmpty() {
+        SelenideElement form = $(".form");
         form.$("[data-test-id=name] input").setValue("Петрова Виктория");
         form.$("[data-test-id=agreement]").click();
         form.$("button").click();
-        $(".input_invalid").$(".input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+        $("[data-test-id=phone].input_invalid .input__sub")
+                .shouldHave(exactText("Поле обязательно для заполнения"));
     }
 
     @Test
-    void shouldTestPhoneWithoutPlus() {
-        open("http://localhost:9999/");
-        SelenideElement form = $(".form.form.form_size_m.form_theme_alfa-on-white");
+    void shouldGetErrorWhenPhoneWithoutPlus() {
+        SelenideElement form = $(".form");
         form.$("[data-test-id=name] input").setValue("Петрова Виктория");
         form.$("[data-test-id=phone] input").setValue("89878888888");
         form.$("[data-test-id=agreement]").click();
         form.$("button").click();
-        $(".input_invalid").$(".input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678. "));
+        $("[data-test-id=phone].input_invalid .input__sub")
+                .shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678. "));
     }
 
     @Test
-    void shouldTestPhoneWithRandomNumbers() {
-        open("http://localhost:9999/");
-        SelenideElement form = $(".form.form.form_size_m.form_theme_alfa-on-white");
+    void shouldGetErrorWhenPhoneWithRandomNumbers() {
+        SelenideElement form = $(".form");
         form.$("[data-test-id=name] input").setValue("Петрова Виктория");
         form.$("[data-test-id=phone] input").setValue("543536");
         form.$("[data-test-id=agreement]").click();
         form.$("button").click();
-        $(".input_invalid").$(".input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678. "));
+        $("[data-test-id=phone].input_invalid .input__sub")
+                .shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678. "));
     }
 
     @Test
-    void shouldTestPhoneWithLetters() {
-        open("http://localhost:9999/");
-        SelenideElement form = $(".form.form.form_size_m.form_theme_alfa-on-white");
+    void shouldGetErrorWhenPhoneWithLetters() {
+        SelenideElement form = $(".form");
         form.$("[data-test-id=name] input").setValue("Петрова Виктория");
         form.$("[data-test-id=phone] input").setValue("frghyys");
         form.$("[data-test-id=agreement]").click();
         form.$("button").click();
-        $(".input_invalid").$(".input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678. "));
+        $("[data-test-id=phone].input_invalid .input__sub")
+                .shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678. "));
     }
 
     @Test
-    void shouldTestPhoneWithoutCheckbox() {
-        open("http://localhost:9999/");
-        SelenideElement form = $(".form.form.form_size_m.form_theme_alfa-on-white");
+    void shouldGetErrorWhenNotClickCheckbox() {
+        SelenideElement form = $(".form");
         form.$("[data-test-id=name] input").setValue("Петрова Виктория");
         form.$("[data-test-id=phone] input").setValue("+79878888888");
         form.$("button").click();
-        $(".input_invalid").$("input").shouldHave(type("checkbox"));
+//        $(".input_invalid").$("input")
+        $("[data-test-id=agreement].input_invalid input")
+                .shouldHave(type("checkbox"));
     }
 }
--- src/test/java/ru/netology/web/CardOrderTest.java
+++ src/test/java/ru/netology/web/CardOrderTest.java
@@ -1,6 +1,7 @@
 package ru.netology.web;
 
 import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.exactText;
@@ -9,47 +10,56 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class CardOrderTest {
 
+    @BeforeEach
+    void setup() {
+        open("http://localhost:9999/");
+    }
+
     @Test
     void shouldTestSingleNameAndSurname() {
-        open("http://localhost:9999/");
-        SelenideElement form = $(".form.form.form_size_m.form_theme_alfa-on-white");
+        SelenideElement form = $(".form");
         form.$("[data-test-id=name] input").setValue("Иванова Мария");
         form.$("[data-test-id=phone] input").setValue("+79878888888");
         form.$("[data-test-id=agreement]").click();
         form.$("button").click();
-        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+        $("[data-test-id=order-success]")
+                .shouldHave(exactText("Ваша заявка успешно отправлена!" +
+                        " Наш менеджер свяжется с вами в ближайшее время."));
     }
 
     @Test
     void shouldTestSingleNameDoubleSurname() {
-        open("http://localhost:9999/");
-        SelenideElement form = $(".form.form.form_size_m.form_theme_alfa-on-white");
+        SelenideElement form = $(".form");
         form.$("[data-test-id=name] input").setValue("Иванова-Петрова Мария");
         form.$("[data-test-id=phone] input").setValue("+79878888888");
         form.$("[data-test-id=agreement]").click();
         form.$("button").click();
-        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+        $("[data-test-id=order-success]")
+                .shouldHave(exactText("Ваша заявка успешно отправлена!" +
+                        " Наш менеджер свяжется с вами в ближайшее время."));
     }
 
     @Test
     void shouldTestDoubleNameSingleSurname() {
-        open("http://localhost:9999/");
-        SelenideElement form = $(".form.form.form_size_m.form_theme_alfa-on-white");
+        SelenideElement form = $(".form");
         form.$("[data-test-id=name] input").setValue("Иванова Мария-Виктория");
         form.$("[data-test-id=phone] input").setValue("+79878888888");
         form.$("[data-test-id=agreement]").click();
         form.$("button").click();
-        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+        $("[data-test-id=order-success]")
+                .shouldHave(exactText("Ваша заявка успешно отправлена!" +
+                        " Наш менеджер свяжется с вами в ближайшее время."));
     }
 
     @Test
     void shouldTestDoubleNameAndSurname() {
-        open("http://localhost:9999/");
-        SelenideElement form = $(".form.form.form_size_m.form_theme_alfa-on-white");
+        SelenideElement form = $(".form");
         form.$("[data-test-id=name] input").setValue("Иванова-Петрова Мария-Виктория");
         form.$("[data-test-id=phone] input").setValue("+79878888888");
         form.$("[data-test-id=agreement]").click();
         form.$("button").click();
-        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+        $("[data-test-id=order-success]")
+                .shouldHave(exactText("Ваша заявка успешно отправлена!" +
+                        " Наш менеджер свяжется с вами в ближайшее время."));
     }
 }
